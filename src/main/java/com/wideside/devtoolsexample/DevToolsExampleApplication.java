7cf9ad48693488129d7e41692f14de215aedeabe
Gavi <gavino.pintus@gmail.com>
2018-11-26 22:23:04 +0100

    First commit

---
+++ src/main/java/com/wideside/devtoolsexample/DevToolsExampleApplication.java
@@ -0,0 +1,12 @@
+package com.wideside.devtoolsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevToolsExampleApplication.class, args);
+	}
+}
---
+++ src/main/java/com/wideside/devtoolsexample/DummyRestController.java
@@ -0,0 +1,13 @@
+package com.wideside.devtoolsexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DummyRestController {
+
+    @GetMapping(path = "/dummyString")
+    public String getDummyString(){
+        return "ciao un cazzo";
+    }
+}
